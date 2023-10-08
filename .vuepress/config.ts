cc0b22cf8008043757d49ab7cbb4cce348170a65
OuHao <ouhao0326@gmail.com>
2023-10-08 21:35:12 +0800

    feat: 个性化配置

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -3,96 +3,97 @@ import type { DefaultThemeOptions } from "vuepress";
 import recoTheme from "vuepress-theme-reco";
 
 export default defineUserConfig({
-  title: "vuepress-theme-reco",
-  description: "Just playing around",
+  title: "ouHaoBlog",
+  description: "OuHao's personal blog.",
+  base: "/Blog/",
   theme: recoTheme({
     style: "@vuepress-reco/style-default",
     logo: "/logo.png",
-    author: "reco_luan",
+    author: "ouHao",
     authorAvatar: "/head.png",
-    docsRepo: "https://github.com/vuepress-reco/vuepress-theme-reco-next",
-    docsBranch: "main",
-    docsDir: "example",
+    // docsRepo: "https://github.com/vuepress-reco/vuepress-theme-reco-next",
+    // docsBranch: "main",
+    // docsDir: "example",
     lastUpdatedText: "",
     // series 为原 sidebar
-    series: {
-      "/docs/theme-reco/": [
-        {
-          text: "module one",
-          children: ["home", "theme"],
-        },
-        {
-          text: "module two",
-          children: ["api", "plugin"],
-        },
-      ],
-    },
+    // series: {
+    //   "/docs/theme-reco/": [
+    //     {
+    //       text: "module one",
+    //       children: ["home", "theme"],
+    //     },
+    //     {
+    //       text: "module two",
+    //       children: ["api", "plugin"],
+    //     },
+    //   ],
+    // },
     navbar: [
       { text: "Home", link: "/" },
-      { text: "Categories", link: "/categories/reco/1/" },
-      { text: "Tags", link: "/tags/tag1/1/" },
-      {
-        text: "Docs",
-        children: [
-          { text: "vuepress-reco", link: "/docs/theme-reco/theme" },
-          { text: "vuepress-theme-reco", link: "/blogs/other/guide" },
-        ],
-      },
+      { text: "Categories", link: "/categories/sheying/1/" },
+      { text: "Tags", link: "/tags/lvxing/1/" },
+      // {
+      //   text: "Docs",
+      //   children: [
+      //     { text: "vuepress-reco", link: "/docs/theme-reco/theme" },
+      //     { text: "vuepress-theme-reco", link: "/blogs/other/guide" },
+      //   ],
+      // },
     ],
-    bulletin: {
-      body: [
-        {
-          type: "text",
-          content: `🎉🎉🎉 reco 主题 2.x 已经接近 Beta 版本，在发布 Latest 版本之前不会再有大的更新，大家可以尽情尝鲜了，并且希望大家在 QQ 群和 GitHub 踊跃反馈使用体验，我会在第一时间响应。`,
-          style: "font-size: 12px;",
-        },
-        {
-          type: "hr",
-        },
-        {
-          type: "title",
-          content: "QQ 群",
-        },
-        {
-          type: "text",
-          content: `
-          <ul>
-            <li>QQ群1：1037296104</li>
-            <li>QQ群2：1061561395</li>
-            <li>QQ群3：962687802</li>
-          </ul>`,
-          style: "font-size: 12px;",
-        },
-        {
-          type: "hr",
-        },
-        {
-          type: "title",
-          content: "GitHub",
-        },
-        {
-          type: "text",
-          content: `
-          <ul>
-            <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/issues">Issues<a/></li>
-            <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/discussions/1">Discussions<a/></li>
-          </ul>`,
-          style: "font-size: 12px;",
-        },
-        {
-          type: "hr",
-        },
-        {
-          type: "buttongroup",
-          children: [
-            {
-              text: "打赏",
-              link: "/docs/others/donate.html",
-            },
-          ],
-        },
-      ],
-    },
+    // bulletin: {
+    //   body: [
+    //     {
+    //       type: "text",
+    //       content: `🎉🎉🎉 reco 主题 2.x 已经接近 Beta 版本，在发布 Latest 版本之前不会再有大的更新，大家可以尽情尝鲜了，并且希望大家在 QQ 群和 GitHub 踊跃反馈使用体验，我会在第一时间响应。`,
+    //       style: "font-size: 12px;",
+    //     },
+    //     {
+    //       type: "hr",
+    //     },
+    //     {
+    //       type: "title",
+    //       content: "QQ 群",
+    //     },
+    //     {
+    //       type: "text",
+    //       content: `
+    //       <ul>
+    //         <li>QQ群1：1037296104</li>
+    //         <li>QQ群2：1061561395</li>
+    //         <li>QQ群3：962687802</li>
+    //       </ul>`,
+    //       style: "font-size: 12px;",
+    //     },
+    //     {
+    //       type: "hr",
+    //     },
+    //     {
+    //       type: "title",
+    //       content: "GitHub",
+    //     },
+    //     {
+    //       type: "text",
+    //       content: `
+    //       <ul>
+    //         <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/issues">Issues<a/></li>
+    //         <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/discussions/1">Discussions<a/></li>
+    //       </ul>`,
+    //       style: "font-size: 12px;",
+    //     },
+    //     {
+    //       type: "hr",
+    //     },
+    //     {
+    //       type: "buttongroup",
+    //       children: [
+    //         {
+    //           text: "打赏",
+    //           link: "/docs/others/donate.html",
+    //         },
+    //       ],
+    //     },
+    //   ],
+    // },
     // commentConfig: {
     //   type: 'valie',
     //   // options 与 1.x 的 valineConfig 配置一致
@@ -108,4 +109,5 @@ export default defineUserConfig({
     // },
   }),
   // debug: true,
+  head: [["link", { rel: "icon", href: "/Blog/logo.png" }]],
 });
--- README.md
+++ README.md
@@ -6,24 +6,24 @@ modules:
   - MdContent
   - Footer
 bannerBrand:
-  bgImage: '/bg.svg'
-  title: vuepress-reco
-  description: 一款简洁的 vuepress 博客 & 文档 主题。
-  tagline: vuepress-theme-reco 2.0 继续坚持简洁的风格，所有功能开箱即用，首页模块化组装，使用 tailwindcss 书写样式，将 Vite 作为默认编译器。你只需要负责内容创作，其他请交给我。
-  buttons:
-    - { text: Guide, link: '/docs/guide/introduce' }
-    - { text: Default Style, link: '/docs/style-default-api/introduce', type: 'plain' }
-  socialLinks:
-    - { icon: 'LogoGithub', link: 'https://github.com/vuepress-reco/vuepress-theme-reco' }
+  bgImage: "/bg.svg"
+  title: OuHao's Blog
+  description: 分享生活中的每个瞬间。
+  tagline: Enjoy life , share love.
+  # buttons:
+  #   - { text: Guide, link: "/docs/guide/introduce" }
+  #   - { text: Default Style, link: "/docs/style-default-api/introduce", type: "plain" }
+  # socialLinks:
+  #   - { icon: "LogoGithub", link: "https://github.com/vuepress-reco/vuepress-theme-reco" }
 blog:
   socialLinks:
-    - { icon: 'LogoGithub', link: 'https://github.com/recoluan' }
+    - { icon: "LogoGithub", link: "https://github.com/recoluan" }
 isShowTitleInHome: true
 actionText: About
 actionLink: /views/other/about
 ---
 
-## 快速开始
+<!-- ## 快速开始
 
 **npx**
 
@@ -46,4 +46,4 @@ theme-cli init
 # 初始化，并选择 2.x
 yarn global add @vuepress-reco/theme-cli@1.0.7
 theme-cli init
-```
+``` -->

72bece4943160bb312971df81bd24f32be18f04e
OuHao <ouhao0326@gmail.com>
2023-10-08 22:01:34 +0800

    fix: 去除dist文件

--- .vuepress/dist/assets/index.html-9c31c1df.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-b1266224","path":"/categories/sheying/1/","title":"","lang":"en-US","frontmatter":{"layout":"Categories"},"headers":[],"git":{},"filePathRelative":null}');export{e as data};
